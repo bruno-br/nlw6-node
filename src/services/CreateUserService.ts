1b067ff51d9fd502e73fac529110dd46ef340869
bruno-br <bruno.brasilf@gmail.com>
2021-06-24 19:27:53 -0300

    feat: add password field to user

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -5,10 +5,11 @@ interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     if (!email) {
@@ -25,6 +26,7 @@ class CreateUserService {
       name,
       email,
       admin,
+      password,
     });
 
     usersRepository.save(user);
