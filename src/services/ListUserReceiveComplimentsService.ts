60751ef9924a5c3ca3b822b50ae2a074b6906de7
bruno-br <bruno.brasilf@gmail.com>
2021-06-25 23:05:33 -0300

    feat: create service to list all compliments received

---
+++ src/services/ListUserReceiveComplimentsService.ts
@@ -0,0 +1,17 @@
+import { getCustomRepository } from "typeorm";
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories";
+
+class ListUserReceiveComplimentsService {
+  async execute(user_id: string) {
+    const complimentsRepositories = getCustomRepository(
+      ComplimentsRepositories
+    );
+    const compliments = await complimentsRepositories.find({
+      user_receiver: user_id,
+    });
+
+    return compliments;
+  }
+}
+
+export { ListUserReceiveComplimentsService };
